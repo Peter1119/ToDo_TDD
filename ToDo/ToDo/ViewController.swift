94b3c0b434eeebe38e7cb14a3cffd7e71dbfa72c
홍석현 <ghdtjrgus333@naver.com>
2024-09-12 17:09:25 +0900

    init. 프로젝트 생성

---
+++ ToDo/ToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by 홍석현 on 9/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
