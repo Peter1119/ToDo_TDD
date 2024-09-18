bb0aca1db1d318517149ed75a5e514f35a4de2b8
홍석현 <ghdtjrgus333@naver.com>
2024-09-18 20:17:05 +0900

    move. 파일 폴더링 정리 및 code base view로 lauch하도록 수정

---
+++ ToDo/ToDo/App/SceneDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  SceneDelegate.swift
+//  ToDo
+//
+//  Created by 홍석현 on 9/12/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        // UIWindow 인스턴스 생성
+        let window = UIWindow(windowScene: windowScene)
+        
+        let rootViewController = ViewController()
+        window.rootViewController = rootViewController
+        
+        // window 설정 및 표시
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
+
